08a39b16484f5fe3dcf7cc629571a38e4f5ba508
DUHA <duha160780@gmail.com>
2024-11-25 13:41:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from unittest import removeResult
+while 1 > 0 :
+    n = int (input('введите число от 3 до 20 : '))
+    if n < 3 or n > 20:
+        print('неправильно введено число')
+    if n == 3:
+        print('result-',12)
+    if n == 4:
+        print ('result-',13)
+    if n == 5:
+        print ('result-',1423)
+    if n == 6:
+        print ('result-',121524)
+    if n == 7:
+        print ('result-',162534)
+    if n == 8:
+        print ('result-',13172635)
+    if n == 9:
+        print ('result-', 1218273645)
+    if n == 10:
+        print ('result-',141923283746)
+    if n == 11:
+        print ('result-', 11029384756)
+    if n == 12:
+        print ('result-',12131511124210394857)
+    if n == 13:
+        print ('result-',112211310495867)
+    if n == 14:
+        print ('result-',1611325212343114105968)
+    if n == 15:
+        print ('result-',1214114232133124115106978)
+    if n == 16:
+        print ('result-',1317115262143531341251161079)
+    if n == 17:
+        print ('result-',11621531441351261171089)
+    if n == 18:
+        print ('result-',12151811724272163631545414513612711810)
+    if n == 19:
+        print ('result-', 118217316415514613712811910)
+    if n == 20:
+        print ('result-',13141911923282183731746416515614713812911)
+
+
